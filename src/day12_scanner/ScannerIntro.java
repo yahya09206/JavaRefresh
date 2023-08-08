8620c94ddd6d0fa585a359162fd481d2d816051d
Yahya <yahyahussein91@gmail.com>
2023-08-07 21:37:21 -0700

    done with scanner intro

--- src/day12_scanner/ScannerIntro.java
+++ src/day12_scanner/ScannerIntro.java
@@ -3,32 +3,19 @@ package day12_scanner;
 import java.util.Scanner;
 
 public class ScannerIntro {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("What is the first number");
-        int num1 = scan.nextInt();
-        System.out.println("What is the second number");
-        int num2 = scan.nextInt();
+        System.out.println("What is your first number?");
+        int num1 = input.nextInt();
 
-        System.out.println("Those two numbers added equals");
-        int addition = num1 + num2;
-        System.out.println(addition);
+        System.out.println("What is your second number?");
+        int num2 = input.nextInt();
 
-        System.out.println("Those two numbers subtracted equals");
-        int subtraction = num1 - num2;
-        System.out.println(subtraction);
-
-        System.out.println("Those two numbers multiplied equals");
-        int multiplication = num1 * num2;
-        System.out.println(multiplication);
-
-        System.out.println("Those two numbers divided equals");
-        int division = num1 / num2;
-        System.out.println(division);
-
-
-
+        System.out.println("Total of both numbers: " + (num1 + num2));
+        System.out.println("Total of both numbers: " + (num1 - num2));
+        System.out.println("Total of both numbers: " + (num1 * num2));
+        System.out.println("Total of both numbers: " + (num1 / num2));
     }
 }
