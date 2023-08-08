96802afb14fb6b3c113644a5b2c81636f315c954
Yahya <yahyahussein91@gmail.com>
2023-08-07 21:43:49 -0700

    finished rectangle

--- src/day12_scanner/Rectangle.java
+++ src/day12_scanner/Rectangle.java
@@ -1,8 +1,20 @@
 package day12_scanner;
 
+import java.util.Scanner;
+
 public class Rectangle {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter the width of rectangle");
+        int width = input.nextInt();
+        int length = input.nextInt();
+
+        int area = width * length;
+        int perimeter = (width + length) * 2;
+
+        System.out.println("area = " + area);
+        System.out.println("perimeter = " + perimeter);
     }
 }
