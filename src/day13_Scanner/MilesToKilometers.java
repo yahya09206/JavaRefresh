2b01593c77347d52151ec2c0429c38a19a2bce86
Yahya <yahyahussein91@gmail.com>
2023-08-08 20:06:27 -0700

    starting mileage converter

---
+++ src/day13_Scanner/MilesToKilometers.java
@@ -0,0 +1,14 @@
+package day13_Scanner;
+
+import java.util.Scanner;
+
+public class MilesToKilometers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter miles");
+        int mileage = scanner.nextInt();
+
+    }
+}
