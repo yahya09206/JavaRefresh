6fdfc0925a0e3cd38468ff1b73f872feca9601eb
Yahya <yahyahussein91@gmail.com>
2023-08-08 20:04:29 -0700

    finished with money converter

--- src/day13_Scanner/CentsToDollars.java
+++ src/day13_Scanner/CentsToDollars.java
@@ -6,5 +6,17 @@ public class CentsToDollars {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Cents");
+        int cents = scanner.nextInt();
+        int dollars = cents / 100;
+        int remainder = cents % 100;
+
+        if (remainder == 0){
+            System.out.println(cents + " is equal to " + dollars + " dollars");
+        }else {
+            System.out.println(cents + " is equal to " + dollars + " dollars and " + remainder + " cents");
+        }
+
+        System.out.println();
     }
 }
