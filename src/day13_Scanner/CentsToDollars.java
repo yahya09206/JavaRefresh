e00c601d304583cbce56092e8fef32413e526791
Yahya <yahyahussein91@gmail.com>
2023-08-08 19:57:35 -0700

    scanner continued

--- src/day13_Scanner/CentsToDollars.java
+++ src/day13_Scanner/CentsToDollars.java
@@ -5,20 +5,6 @@ import java.util.Scanner;
 public class CentsToDollars {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter Cents");
-        int cents = scan.nextInt(); // 225 == 2.25 --> cut off the decimal
-        int dollarAmount = cents / 100; // 2
-        int remainder = cents % 100; // 25
-
-        if (cents >= 0){
-            if (remainder == 0){
-                System.out.println(cents + " is equal to " + dollarAmount + " dollars");
-            }else {
-                System.out.println(cents + " is equal to " + dollarAmount + " dollars and " + remainder + " cents");
-            }
-        }else{
-            System.out.println("Input is invalid");
-        }
+        Scanner scanner = new Scanner(System.in);
     }
 }
