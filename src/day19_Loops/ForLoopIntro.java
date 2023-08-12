c030dbd2cde730ff024110947897b0476b120cc9
Yahya <yahyahussein91@gmail.com>
2023-08-11 21:31:42 -0700

    for loop intro

---
+++ src/day19_Loops/ForLoopIntro.java
@@ -0,0 +1,15 @@
+package day19_Loops;
+
+public class ForLoopIntro {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+    }
+
+}
