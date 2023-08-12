82daa7113d20e35ef35619a0c48ea72cc2a51e5e
Yahya <yahyahussein91@gmail.com>
2023-08-12 13:49:52 -0700

    fixed error

--- src/day20_LoopsContinued/FactorialNumber.java
+++ src/day20_LoopsContinued/FactorialNumber.java
@@ -3,6 +3,13 @@ package day20_LoopsContinued;
 public class FactorialNumber {
     public static void main(String[] args) {
 
+        int number = 3;
+        int result = 1;
 
+        for (int i = number; 1 <= i; i--){
+            result *= i;
+        }
+
+        System.out.println(result);
     }
 }
