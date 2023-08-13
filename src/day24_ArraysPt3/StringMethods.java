d0949326120a7e4400ae642bdedff504c7b81233
Yahya <yahyahussein91@gmail.com>
2023-08-13 16:50:13 -0700

    split method

---
+++ src/day24_ArraysPt3/StringMethods.java
@@ -0,0 +1,12 @@
+package day24_ArraysPt3;
+
+public class StringMethods {
+    public static void main(String[] args) {
+
+        String senntence = "I love learning java programming language";
+
+        senntence.split(" ");
+
+
+    }
+}
