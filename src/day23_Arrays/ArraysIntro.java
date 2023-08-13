349a97d8e761efff1952f8ffc73e005d99c90bfc
Yahya <yahyahussein91@gmail.com>
2023-08-12 21:19:55 -0700

    intro to arrays

--- src/day23_Arrays/ArraysIntro.java
+++ src/day23_Arrays/ArraysIntro.java
@@ -7,14 +7,6 @@ public class ArraysIntro {
 
         String[] group1 = {"Feruza", "Yuliang", "Tamara", "Ahmet"};
 
-        // retrieve data from array:
-        System.out.println("group1[1] = " + group1[1]);
-        System.out.println("group1[2] = " + group1[2]);
-
-        System.out.println("-----------------------------------------------------");
-        for (int i = 0; i <= group1.length; i++){
-            System.out.println(group1[i]);
-        }
-        System.out.println("Arrays.toString(group1) = " + Arrays.toString(group1));
+        System.out.println(Arrays.toString(group1));
     }
 }
