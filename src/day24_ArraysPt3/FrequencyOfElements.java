78615350e7b997e8834a07a3c6a58436417d882e
Yahya <yahyahussein91@gmail.com>
2023-08-13 16:38:20 -0700

    frquency of elements

---
+++ src/day24_ArraysPt3/FrequencyOfElements.java
@@ -0,0 +1,10 @@
+package day24_ArraysPt3;
+
+public class FrequencyOfElements {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 1, 2, 3, 3, 4};
+        int element = arr[0];
+        int count = 0;
+    }
+}
