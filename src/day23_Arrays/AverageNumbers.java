25a946b00e032a5fb81afc2930fa2139f18e14db
Yahya <yahyahussein91@gmail.com>
2023-08-12 22:19:19 -0700

    highest and lowest scores

---
+++ src/day23_Arrays/AverageNumbers.java
@@ -0,0 +1,14 @@
+package day23_Arrays;
+
+public class AverageNumbers {
+    public static void main(String[] args) {
+
+        int[] numbers = {85, 70, 95, 90, 100};
+
+        String[] names = {"Mike", "Adam", "Tonny", "John", "Ammy"};
+
+        for (int i = 0; i <= names.length - 1; i++){
+            System.out.println(names[i] + numbers);
+        }
+    }
+}
--- src/day23_Arrays/HighestAndLowestScores.java
+++ src/day23_Arrays/HighestAndLowestScores.java
@@ -3,9 +3,35 @@ package day23_Arrays;
 public class HighestAndLowestScores {
     public static void main(String[] args) {
 
-        int[] numbers = {85, 70, 95, 90, 100}:
+        int[] numbers = {85, 70, 95, 90, 100};
         String[] names = {"Mike", "Adam", "Tonny", "John", "Ammy"};
 
+        int maxScore = 0;
+        String maxName = "";
 
+        int minScore = numbers[0];
+        String minName = "";
+
+        for (int i = 0; i <= names.length - 1; i++){
+            String eachName = names[i];
+            int eachScore = numbers[i];
+
+            if (eachScore > maxScore){
+                maxScore = eachScore;
+                maxName = eachName;
+            }
+
+            if (eachScore < minScore){
+                minScore = eachScore;
+                minName = eachName;
+
+            }
+        }
+
+        System.out.println("maxScore = " + maxScore);
+        System.out.println("maxName = " + maxName);
+
+        System.out.println("minScore = " + minScore);
+        System.out.println("minName = " + minName);
     }
 }
