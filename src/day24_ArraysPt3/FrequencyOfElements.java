9bb26046c384751ba8c72bfbc95d4cdc046a075e
Yahya <yahyahussein91@gmail.com>
2023-08-13 16:44:49 -0700

    reversing an array

--- src/day24_ArraysPt3/FrequencyOfElements.java
+++ src/day24_ArraysPt3/FrequencyOfElements.java
@@ -4,7 +4,21 @@ public class FrequencyOfElements {
     public static void main(String[] args) {
 
         int[] arr = {1, 1, 2, 3, 3, 4};
-        int element = arr[0];
-        int count = 0;
+
+        for (int j = 0; j <= arr.length - 1; j++){ // loop to iterate thru array
+
+            int element = arr[j];
+            int count = 0;
+
+            for (int i = 0; i <= arr.length - 1; i++){// loop to check each element
+                if (arr[i] == element){
+                    count++;
+                }
+            }
+
+            if (count == 1){
+                System.out.println(element + " ");
+            }
+        }
     }
 }
---
+++ src/day24_ArraysPt3/ReverseArray.java
@@ -0,0 +1,12 @@
+package day24_ArraysPt3;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5};
+        int[] reversedArray = new int[array.length];
+
+
+
+    }
+}
