2ff48cf6f53811536e2f562343d074c2a569fba8
Yahya <yahyahussein91@gmail.com>
2023-08-12 22:22:18 -0700

    average of numbers

--- src/day23_Arrays/AverageNumbers.java
+++ src/day23_Arrays/AverageNumbers.java
@@ -1,14 +1,23 @@
 package day23_Arrays;
 
+import java.util.Scanner;
+
 public class AverageNumbers {
     public static void main(String[] args) {
 
-        int[] numbers = {85, 70, 95, 90, 100};
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many numbers do you want to enter?");
+        int[] numbers = new int[scan.nextInt()];
+        int sum = 0;
 
-        String[] names = {"Mike", "Adam", "Tonny", "John", "Ammy"};
-
-        for (int i = 0; i <= names.length - 1; i++){
-            System.out.println(names[i] + numbers);
+        for (int i = 0; i <= numbers.length - 1; i++){
+            System.out.println("Enter a number");
+            numbers[i] = scan.nextInt();
+            sum += numbers[i];
         }
+
+        System.out.println("sum = " + sum);
+        double avg = (double) sum / numbers.length;
+        System.out.println("avg = " + avg);
     }
 }
