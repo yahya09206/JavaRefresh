d119e59d7fb2313ceffa78f7ea91ecf858ba1307
Yahya <yahyahussein91@gmail.com>
2023-08-12 22:41:41 -0700

    done for the night

---
+++ src/day23_Arrays/ArraysUtility.java
@@ -0,0 +1,13 @@
+package day23_Arrays;
+
+import java.util.Arrays;
+
+public class ArraysUtility {
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5, 6};
+        System.out.println(array);
+
+        System.out.println("Arrays.toString(array) = " + Arrays.toString(array));
+    }
+}
