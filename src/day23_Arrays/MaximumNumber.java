d91aa2072841bd2fcea0b829295ab92208e0dc44
Yahya <yahyahussein91@gmail.com>
2023-08-12 22:06:21 -0700

    min and max number

---
+++ src/day23_Arrays/MaximumNumber.java
@@ -0,0 +1,25 @@
+package day23_Arrays;
+
+public class MaximumNumber {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 5, 4, 400, 50, 0, -1};
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 0; i <= numbers.length - 1; i++){
+            int each = numbers[i];
+
+            if (each > max){
+                max = each;
+            }
+
+            if (each < min){
+                min = each;
+            }
+        }
+
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+    }
+}
