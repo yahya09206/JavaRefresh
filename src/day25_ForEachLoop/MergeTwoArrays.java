149e4658691c2e6c596ed06b3661ceed267ff254
Yahya <yahyahussein91@gmail.com>
2023-08-13 21:20:56 -0700

    merging three arrays

--- src/day25_ForEachLoop/MergeTwoArrays.java
+++ src/day25_ForEachLoop/MergeTwoArrays.java
@@ -1,13 +1,24 @@
 package day25_ForEachLoop;
 
+import java.util.Arrays;
+
 public class MergeTwoArrays {
     public static void main(String[] args) {
 
         int[] arr1 = {1, 2, 3, 4},
                 arr2 = {5, 6};
 
-        int[] arr = new int[arr1.length + arr2.length];
+        int[] arr3 = new int[arr1.length + arr2.length];
 
+        int j = 0;
+        for (int i = 0; i < arr1.length; i++){
+            arr3[j++] = arr1[i];
+        }
 
+        for (int i = 0; i < arr2.length; i++){
+            arr3[j++] = arr2[i];
+        }
+
+        System.out.println(Arrays.toString(arr3));
     }
 }
