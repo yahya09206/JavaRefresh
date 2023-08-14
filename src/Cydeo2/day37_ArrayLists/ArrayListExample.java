d95f7a3ca69ad3639ea7030e932ae5d12ef1eb48
Yahya <yahyahussein91@gmail.com>
2023-08-13 23:13:25 -0700

    done with arraylist pt 1

--- src/Cydeo2/day37_ArrayLists/ArrayListExample.java
+++ src/Cydeo2/day37_ArrayLists/ArrayListExample.java
@@ -6,6 +6,26 @@ public class ArrayListExample {
     public static void main(String[] args) {
 
         ArrayList<Integer> numbers = new ArrayList<>();
+        System.out.println(numbers);
+
+        // add/assign to an array [index] = value
+        numbers.add(100);
+        System.out.println(numbers);
+
+        numbers.add(-100);
+        System.out.println(numbers);
+        numbers.add(50);
+        numbers.add(30);
+        System.out.println(numbers);
+
+        // arraylist has methods to select values
+        System.out.println(numbers.get(0));
+        System.out.println(numbers.get(1));
+        System.out.println(numbers.get(2));
+        System.out.println(numbers.get(3));
+
+        // size() to find out the length of an arrayList
+        System.out.println(numbers.size());
 
     }
 }
