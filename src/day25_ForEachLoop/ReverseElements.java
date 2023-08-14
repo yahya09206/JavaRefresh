71bce9555cdd961a853f65f26885df12dda8bf16
Yahya <yahyahussein91@gmail.com>
2023-08-13 21:07:30 -0700

    reversing elements

--- src/day25_ForEachLoop/ReverseElements.java
+++ src/day25_ForEachLoop/ReverseElements.java
@@ -1,23 +1,15 @@
 package day25_ForEachLoop;
 
-import java.util.Arrays;
-
 public class ReverseElements {
     public static void main(String[] args) {
 
         String[] array = {"Java", "Python", "C#"};
 
-        for (int j = 0; j < array.length; j++){
-            String element = array[j];
-            String reverse = "";
+        String element = array[0];
+        String reverse = "";
 
-            for (int i = element.length() - 1; i >= 0; i--){
-                reverse += element.charAt(i); // gets the character from the string element
-            }
+        for (int i = element.length() - 1; i >= 0; i--){
 
-            array[j] = reverse;
         }
-
-        System.out.println(Arrays.toString(array));
     }
 }
