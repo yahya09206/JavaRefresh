20b984b8af23f63639754803265aa57d02e71718
Yahya <yahyahussein91@gmail.com>
2023-08-13 21:13:33 -0700

    merging two arrays

---
+++ src/day25_ForEachLoop/MergeTwoArrays.java
@@ -0,0 +1,13 @@
+package day25_ForEachLoop;
+
+public class MergeTwoArrays {
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 2, 3, 4},
+                arr2 = {5, 6};
+
+        int[] arr = new int[arr1.length + arr2.length];
+
+
+    }
+}
--- src/day25_ForEachLoop/ReverseElements.java
+++ src/day25_ForEachLoop/ReverseElements.java
@@ -1,15 +1,21 @@
 package day25_ForEachLoop;
 
+import java.util.Arrays;
+
 public class ReverseElements {
     public static void main(String[] args) {
 
         String[] array = {"Java", "Python", "C#"};
 
-        String element = array[0];
-        String reverse = "";
+        for (int j = 0; j < array.length; j++){
+            String element = array[j];
+            String reverse = "";
 
-        for (int i = element.length() - 1; i >= 0; i--){
-
+            for (int i = element.length() - 1; i >= 0; i--){
+                reverse += element.charAt(i);
+            }
+            array[j] = reverse;
         }
+        System.out.println(Arrays.toString(array));
     }
 }
