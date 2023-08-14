8e845dc6a05d66b8ffe2b8204dc1d49a1796b5eb
Yahya <yahyahussein91@gmail.com>
2023-08-13 21:57:00 -0700

    arraylists

--- src/Cydeo2/day37_ArrayLists/ArrayListExample.java
+++ src/Cydeo2/day37_ArrayLists/ArrayListExample.java
@@ -6,12 +6,6 @@ public class ArrayListExample {
     public static void main(String[] args) {
 
         ArrayList<Integer> numbers = new ArrayList<>();
-        System.out.println(numbers);
-        numbers.add(3);
-        numbers.add(4);
-        System.out.println(numbers);
-        System.out.println(numbers.size());
-
 
     }
 }
