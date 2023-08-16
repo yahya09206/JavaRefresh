ffefed20a56dd664eae0c64c67657660b7cddfec
Yahya <yahyahussein91@gmail.com>
2023-08-15 21:35:43 -0700

    new section on statics

--- src/Cydeo2/day46_Static/AccessInPackage.java
+++ src/Cydeo2/day46_Static/AccessInPackage.java
@@ -3,16 +3,5 @@ package Cydeo2.day46_Static;
 public class AccessInPackage {
     public static void main(String[] args) {
 
-        AccessModifier obj = new AccessModifier();
-        System.out.println(obj.a);
-        System.out.println(obj.b);
-        //System.out.println(obj.c);
-
-        System.out.println(AccessModifier.x);
-        System.out.println(AccessModifier.y);
-        //System.out.println(AccessModifier.z);
-
-        // access all the information
-
     }
 }
